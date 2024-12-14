a9ad1f8a8fba447aff7243610a4769020bcdb73f
Gokul Singh <62161468+Goki27@users.noreply.github.com>
2024-12-14 15:37:03 +0530

    Started Java Basics

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        int a =10;
+        System.out.print(a);
+
+
+        System.out.println("Hello, World!");
+    }
+}
