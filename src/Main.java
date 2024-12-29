0f6bc5a493e21a6e814cc64e71bc6d9022352bf9
Gokul Singh <62161468+Goki27@users.noreply.github.com>
2024-12-29 21:02:08 +0530

    Started Java Basics

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,85 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
 
 
-        int a =10;
-        System.out.print(a);
+        //input 3 nos a b c  output the average of 3 numberrs
 
-        System.out.println("Hello, World!");
 
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
+//        Scanner sc= new Scanner(System.in);
+//
+//        int a=sc.nextInt();
+//        int b=sc.nextInt();
+//        int c=sc.nextInt();
+//
+//
+//        double d=(a+b+c)/3;
+//
+//        System.out.print(d);
+//
+
+
+//2nd
+
+//we have to find area of the square
+
+//
+//        Scanner sc=new Scanner(System.in);
+//       System.out.println("Enter the side of the Square");
+//        int sideofSquare=sc.nextInt();
+//
+//        int areaOfSquare=sideofSquare*sideofSquare;
+//
+//        System.out.print("The area of the square is "+areaOfSquare);
+
+
+//
+
+
+        //3rd
+        // enter cost of 3 items from the user using float data type a pencil a pen and an eraser.Your have to output the total cost of the items back to the
+        //user as their bill
+//
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("Please enter the cost of the Pencil ");
+//        float pencil=sc.nextFloat();
+//        System.out.println("Please enter the cost of the pen");
+//        float  pen= sc.nextFloat();
+//        System.out.println("Please enter the cost of the eraser ");
+//        float eraser=sc.nextFloat();
+//
+//        float bill= pencil+pen+eraser;
+//        System.out.println("This is your total bill "+bill);
+//
+//        float gst= bill*18/100;
+//        System.out.println("Your gst is "+gst);
+//        System.out.print("Your total bill with gst is "+(gst+bill));
+
+
+//
+//        //4th
+//          byte b=4;
+//          char c='a';
+//          short s=512;
+//          int i=1000;
+//          float f=3.14f;
+//          double d=99.644;
+//
+//          Double result=(f*b)+(i%c)-(d*s);
+//
+//          System.out.print(result);
+
+
+
+
+
+
+
+
+
+
+
     }
 }
